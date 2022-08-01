77e54832fde36f2c76f9771ec08a94dd0e6bac55
jahangir <47403280+cloudy4next@users.noreply.github.com>
2022-08-01 19:23:06 +0600

    export function

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -117,7 +117,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
     <script src="{{ asset('js/main.js') }}"></script>
-    {{-- <script>
+     <script>
         $(function() {
             let copyButtonTrans = '{{ trans('global.datatables.copy') }}'
             let csvButtonTrans = '{{ trans('global.datatables.csv') }}'
@@ -207,7 +207,7 @@
 
             $.fn.dataTable.ext.classes.sPageButton = '';
         });
-    </script> --}}
+    </script> 
     @yield('scripts')
 </body>
 
